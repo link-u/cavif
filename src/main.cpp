1e6dd81925ad532c75a47beff397216cb54902b3
psi <ryo.hirafuji@link-u.co.jp>
2020-01-06 19:37:14 +0900

    [WIP] Parsing OBUs.
    Left to do:
     - fill avif::FileBox
     - Implement avif::Writer class
     - Write it to an avif file.
     - Support .bmp inputs.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <aom/aom_codec.h>
 #include <aom/aomcx.h>
 #include <libyuv.h>
+#include <avif/av1/Parser.hpp>
+#include <avif/util/FileLogger.hpp>
+#include <avif/FileBox.hpp>
 
 namespace {
 
@@ -29,13 +32,15 @@ std::string basename(std::string const& path) {
 }
 
 int main(int argc, char** argv) {
+  avif::util::FileLogger log(stdout, stderr, avif::util::Logger::Level::DEBUG);
+
   std::string inputFilename = {};
   std::string outputFilename = {};
   aom_codec_ctx_t codec = {};
   aom_codec_enc_cfg cfg = {};
   aom_codec_iface_t* av1codec = aom_codec_av1_cx();
-  if(!av1codec){
-    std::cerr << ("Failed to get AV1 encoder.");
+  if(!av1codec) {
+    log.fatal("failed to get AV1 encoder.");
   }
   aom_codec_enc_config_default(av1codec, &cfg, 0);
   {
@@ -65,8 +70,7 @@ int main(int argc, char** argv) {
   } else if(endsWidh(inputFilename, ".bmp")) {
 
   } else {
-    std::cerr << "Please give png or bmp file for input" << std::endl;
-    return -3;
+    log.fatal("please give png or bmp file for input");
   }
   uint32_t const width = srcImage.width();
   uint32_t const height = srcImage.height();
@@ -91,33 +95,37 @@ int main(int argc, char** argv) {
   // initialize encoder
   cfg.g_w = width;
   cfg.g_h = height;
+  cfg.encoder_cfg.disable_cdef = 1;
+  cfg.encoder_cfg.disable_dual_filter = 1;
+  cfg.monochrome = 0;
+  cfg.g_profile = 1;
+  cfg.full_still_picture_hdr = 0;
   if(AOM_CODEC_OK != aom_codec_enc_init(&codec, av1codec, &cfg, 0)) {
-    std::cerr << "Failed to initialize encoder." << std::endl;
-    return -2;
+    log.fatal("Failed to initialize encoder.");
   }
 
   aom_image_t img;
   // FIXME: read validate_img() function.
   // https://github.com/link-u/libaom/blob/d0b3d306aebb5ef6cc89f49f56dd7adaee41f696/av1/av1_cx_iface.c#L532
-  if(cfg.monochrome) {
-    aom_img_alloc(&img, AOM_IMG_FMT_I420, width, height, 1);
-    libyuv::ARGBToI420(staging.data(), statingStride, img.planes[0], img.stride[0], img.planes[1], img.stride[1], img.planes[2], img.stride[2], width, height);
-  } else {
-    aom_img_alloc(&img, AOM_IMG_FMT_I444, width, height, 1);
-    libyuv::ARGBToI444(staging.data(), statingStride, img.planes[0], img.stride[0], img.planes[1], img.stride[1], img.planes[2], img.stride[2], width, height);
-  }
+  //aom_img_alloc(&img, AOM_IMG_FMT_I420, width, height, 1);
+  //libyuv::ARGBToI420(staging.data(), statingStride, img.planes[0], img.stride[0], img.planes[1], img.stride[1], img.planes[2], img.stride[2], width, height);
+  aom_img_alloc(&img, AOM_IMG_FMT_I444, width, height, 1);
+  libyuv::ARGBToI444(staging.data(), statingStride, img.planes[0], img.stride[0], img.planes[1], img.stride[1], img.planes[2], img.stride[2], width, height);
+
+  std::vector<std::vector<uint8_t>> packets;
 
   { // encode a frame
     aom_codec_cx_pkt_t const* pkt;
     aom_codec_iter_t iter = nullptr;
     aom_codec_err_t const res = aom_codec_encode(&codec, &img, 0, 1, AOM_EFLAG_FORCE_KF);
     if (res != AOM_CODEC_OK) {
-      throw std::runtime_error("failed to encode a key frame");
+      log.fatal("failed to encode a key frame");
     }
     while ((pkt = aom_codec_get_cx_data(&codec, &iter)) != nullptr) {
       if (pkt->kind == AOM_CODEC_CX_FRAME_PKT) {
         auto& frame = pkt->data.frame;
-        // TODO: parse OBU and store data
+        auto const beg = reinterpret_cast<uint8_t*>(frame.buf);
+        packets.emplace_back(std::vector<uint8_t>(beg, beg + frame.sz));
       }
       break;
     }
@@ -129,14 +137,15 @@ int main(int argc, char** argv) {
       aom_codec_cx_pkt_t const* pkt = nullptr;
       const aom_codec_err_t res = aom_codec_encode(&codec, nullptr, -1, 1, 0);
       if (res != AOM_CODEC_OK) {
-        throw std::runtime_error("failed to encode");
+        log.fatal("failed to flushing encoder");
       }
       bool gotPkt = false;
       while ((pkt = aom_codec_get_cx_data(&codec, &iter)) != nullptr) {
         gotPkt = true;
         if (pkt->kind == AOM_CODEC_CX_FRAME_PKT) {
           auto& frame = pkt->data.frame;
-          // TODO: parse OBU and store data
+          auto const beg = reinterpret_cast<uint8_t*>(frame.buf);
+          packets.emplace_back(std::vector<uint8_t>(beg, beg + frame.sz));
         }
       }
       cont &= gotPkt;
@@ -144,8 +153,26 @@ int main(int argc, char** argv) {
   }
   aom_img_free(&img);
   if (aom_codec_destroy(&codec) != AOM_CODEC_OK) {
-    std::cerr << "Failed to destroy codec.";
+    log.error("Failed to destroy codec.");
     return -1;
+  }
+  if(packets.empty()) {
+    log.error("no packats to out.");
+    return -1;
+  }
+  avif::FileBox fileBox;
+  {
+    avif::av1::Parser::Result obus = avif::av1::Parser(log, packets[0]).parse();
+    for(avif::av1::Parser::Result::Packet const& packet : obus.packets()) {
+      switch (packet.type()) {
+        case avif::av1::Header::Type::SequenceHeader:
+          break;
+        case avif::av1::Header::Type::Frame:
+          break;
+        default:
+          break;
+      }
+    }
   }
   return 0;
 }
