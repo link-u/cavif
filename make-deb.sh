579e389f3cc7f3d02ce7099a786b1cd8e9ad7032
psi <ryo.hirafuji@link-u.co.jp>
2020-01-27 16:42:24 +0900

    fix build script

--- make-deb.sh
+++ make-deb.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+set -e
 fakeroot debian/rules clean
 fakeroot debian/rules binary
+# workaround. external/libpng will be dirty after making debian packages.
 env --chdir=external/libpng git reset --hard
 mv ../cavif_*.deb ../cavif-dbgsym_*.ddeb .
