3f72d53aeecd69350c888c1208625fc4b152f4a1
psi <ryo.hirafuji@link-u.co.jp>
2020-01-28 15:35:20 +0900

    move deb-package script

--- scripts/build-debian-package.sh
+++ scripts/build-debian-package.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 set -e
+
+BASE_DIR=$(cd $(dirname $0); cd ..; pwd)
+cd ${BASE_DIR}
+
 fakeroot debian/rules clean
 fakeroot debian/rules binary
 # workaround. external/libpng will be dirty after making debian packages.
