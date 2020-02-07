635b0ab0679781d0222280d46ecd322b88ae69f0
psi <ryo.hirafuji@link-u.co.jp>
2020-02-08 03:01:33 +0900

    support full color range transformation

--- src/img/Convert.hpp
+++ src/img/Convert.hpp
@@ -8,26 +8,26 @@
 #include "avif/img/Image.hpp"
 #include "avif/img/Conversion.hpp"
 
-template <uint8_t rgbBits, uint8_t yuvBits>
+template <uint8_t rgbBits, uint8_t yuvBits, bool isFullRange>
 void convert(avif::img::Image<rgbBits>& src, aom_image& dst) {
   switch (dst.fmt) {
     case AOM_IMG_FMT_I420:
     case AOM_IMG_FMT_I42016:
-      avif::img::FromRGB<rgbBits, yuvBits>().toI420(src,
+      avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI420(src,
                                                     dst.planes[0], dst.stride[0],
                                                     dst.planes[1], dst.stride[1],
                                                     dst.planes[2], dst.stride[2]);
       break;
     case AOM_IMG_FMT_I422:
     case AOM_IMG_FMT_I42216:
-      avif::img::FromRGB<rgbBits, yuvBits>().toI422(src,
+      avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI422(src,
                                                     dst.planes[0], dst.stride[0],
                                                     dst.planes[1], dst.stride[1],
                                                     dst.planes[2], dst.stride[2]);
       break;
     case AOM_IMG_FMT_I444:
     case AOM_IMG_FMT_I44416:
-      avif::img::FromRGB<rgbBits, yuvBits>().toI444(src,
+      avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI444(src,
                                                     dst.planes[0], dst.stride[0],
                                                     dst.planes[1], dst.stride[1],
                                                     dst.planes[2], dst.stride[2]);
@@ -38,19 +38,35 @@ void convert(avif::img::Image<rgbBits>& src, aom_image& dst) {
 }
 
 
-template <size_t BitsPerPixel>
-void convert(avif::img::Image<BitsPerPixel>& src, aom_image& dst, int const yuvBits) {
-  switch (yuvBits) {
-    case 8:
-      convert<BitsPerPixel, 8>(src, dst);
-      break;
-    case 10:
-      convert<BitsPerPixel, 10>(src, dst);
-      break;
-    case 12:
-      convert<BitsPerPixel, 12>(src, dst);
-      break;
-    default:
-      throw std::invalid_argument(fmt::format("Unsupported YUV bit-depth: {}", dst.bit_depth));
+template <size_t rgbBits>
+void convert(avif::img::Image<rgbBits>& src, aom_image& dst, int const yuvBits) {
+  if (dst.range == AOM_CR_STUDIO_RANGE) {
+    switch (yuvBits) {
+      case 8:
+        convert<rgbBits, 8, false>(src, dst);
+        break;
+      case 10:
+        convert<rgbBits, 10, false>(src, dst);
+        break;
+      case 12:
+        convert<rgbBits, 12, false>(src, dst);
+        break;
+      default:
+        throw std::invalid_argument(fmt::format("Unsupported YUV bit-depth: {}", dst.bit_depth));
+    }
+  } else {
+    switch (yuvBits) {
+      case 8:
+        convert<rgbBits, 8, true>(src, dst);
+        break;
+      case 10:
+        convert<rgbBits, 10, true>(src, dst);
+        break;
+      case 12:
+        convert<rgbBits, 12, true>(src, dst);
+        break;
+      default:
+        throw std::invalid_argument(fmt::format("Unsupported YUV bit-depth: {}", dst.bit_depth));
+    }
   }
 }
