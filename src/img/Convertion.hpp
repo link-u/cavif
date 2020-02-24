57b918ece3e59ff043dc1410d1d6d6c96b417255
psi <ryo.hirafuji@link-u.co.jp>
2020-02-25 05:38:47 +0900

    Support monochrome input correctly

--- src/img/Convertion.hpp
+++ src/img/Convertion.hpp
@@ -9,37 +9,55 @@
 
 namespace detail {
 
-template <uint8_t rgbBits, uint8_t yuvBits, bool isFullRange>
+template <uint8_t rgbBits, uint8_t yuvBits, bool fromMonoRGB, bool isFullRange>
 void convertImage(avif::img::Image<rgbBits>& src, aom_image& dst) {
   if(dst.monochrome) {
-    avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI400(src,
-                                                               dst.planes[0], dst.stride[0]);
+    avif::img::FromRGB<rgbBits, yuvBits, fromMonoRGB, isFullRange>().toI400(src,
+                                                                            dst.planes[0], dst.stride[0]);
   } else {
     switch (dst.fmt) {
       case AOM_IMG_FMT_I420:
       case AOM_IMG_FMT_I42016:
-        avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI420(src,
-                                                                   dst.planes[0], dst.stride[0],
-                                                                   dst.planes[1], dst.stride[1],
-                                                                   dst.planes[2], dst.stride[2]);
+        avif::img::FromRGB<rgbBits, yuvBits, fromMonoRGB, isFullRange>().toI420(src,
+                                                                                dst.planes[0], dst.stride[0],
+                                                                                dst.planes[1], dst.stride[1],
+                                                                                dst.planes[2], dst.stride[2]);
         break;
       case AOM_IMG_FMT_I422:
       case AOM_IMG_FMT_I42216:
-        avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI422(src,
-                                                                   dst.planes[0], dst.stride[0],
-                                                                   dst.planes[1], dst.stride[1],
-                                                                   dst.planes[2], dst.stride[2]);
+        avif::img::FromRGB<rgbBits, yuvBits, fromMonoRGB, isFullRange>().toI422(src,
+                                                                                dst.planes[0], dst.stride[0],
+                                                                                dst.planes[1], dst.stride[1],
+                                                                                dst.planes[2], dst.stride[2]);
         break;
       case AOM_IMG_FMT_I444:
       case AOM_IMG_FMT_I44416:
-        avif::img::FromRGB<rgbBits, yuvBits, isFullRange>().toI444(src,
-                                                                   dst.planes[0], dst.stride[0],
-                                                                   dst.planes[1], dst.stride[1],
-                                                                   dst.planes[2], dst.stride[2]);
+        avif::img::FromRGB<rgbBits, yuvBits, fromMonoRGB, isFullRange>().toI444(src,
+                                                                                dst.planes[0], dst.stride[0],
+                                                                                dst.planes[1], dst.stride[1],
+                                                                                dst.planes[2], dst.stride[2]);
         break;
       default:
         throw std::invalid_argument(fmt::format("Unsupported image format: {:08x}", dst.fmt));
     }
+  }
+}
+
+template <uint8_t rgbBits, uint8_t yuvBits, bool fromMonoRGB>
+void convertImage(avif::img::Image<rgbBits>& src, aom_image& dst) {
+  if(dst.range == AOM_CR_FULL_RANGE) {
+    convertImage<rgbBits, yuvBits, fromMonoRGB, true>(src, dst);
+  } else {
+    convertImage<rgbBits, yuvBits, fromMonoRGB, false>(src, dst);
+  }
+}
+
+template <uint8_t rgbBits, uint8_t yuvBits>
+void convertImage(avif::img::Image<rgbBits>& src, aom_image& dst) {
+  if(src.isMonochrome()) {
+    convertImage<rgbBits, yuvBits, true>(src, dst);
+  } else {
+    convertImage<rgbBits, yuvBits, false>(src, dst);
   }
 }
 
@@ -49,37 +67,32 @@ void convertAlpha(avif::img::Image<rgbBits>& src, aom_image& dst) {
     avif::img::FromAlpha<rgbBits, yuvBits, isFullRange>().toI400(src,
                                                                  dst.planes[0], dst.stride[0]);
   } else {
-    throw std::invalid_argument("Alpha image should be monochrome");
+    throw std::invalid_argument("Alpha image should be monochrome. Please add --monochrome option.");
   }
 }
 
+template <uint8_t rgbBits, uint8_t yuvBits>
+void convertAlpha(avif::img::Image<rgbBits>& src, aom_image& dst) {
+  if(dst.range == AOM_CR_FULL_RANGE) {
+    convertAlpha<rgbBits, yuvBits, true>(src, dst);
+  } else {
+    convertAlpha<rgbBits, yuvBits, false>(src, dst);
+  }
 }
 
-template <Config::EncodeTarget target, uint8_t rgbBits, uint8_t yuvBits, bool isFullRange>
-void convert(avif::img::Image<rgbBits>& src, aom_image& dst) {
-  switch (target) {
-    case Config::EncodeTarget::Image:
-      detail::convertImage<rgbBits, yuvBits, isFullRange>(src, dst);
-      break;
-    case Config::EncodeTarget::Alpha:
-      detail::convertAlpha<rgbBits, yuvBits, isFullRange>(src, dst);
-      break;
-    default:
-      throw std::invalid_argument(fmt::format("Unsupported EncodeTarget: {}", target));
-  }
 }
 
 template <Config::EncodeTarget target, uint8_t rgbBits, uint8_t yuvBits>
 void convert(avif::img::Image<rgbBits>& src, aom_image& dst) {
-  switch(dst.range) {
-    case AOM_CR_STUDIO_RANGE:
-      convert<target, rgbBits, yuvBits, false>(src, dst);
+  switch (target) {
+    case Config::EncodeTarget::Image:
+      detail::convertImage<rgbBits, yuvBits>(src, dst);
       break;
-    case AOM_CR_FULL_RANGE:
-      convert<target, rgbBits, yuvBits, true>(src, dst);
+    case Config::EncodeTarget::Alpha:
+      detail::convertAlpha<rgbBits, yuvBits>(src, dst);
       break;
     default:
-      throw std::invalid_argument(fmt::format("Unsupported color range type: {}", dst.range));
+      throw std::invalid_argument(fmt::format("Unsupported EncodeTarget: {}", target));
   }
 }
 
