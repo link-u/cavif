5433455dfb79d3b587a9a31b0dc13d31b255e99d
psi <ryo.hirafuji@link-u.co.jp>
2020-06-29 21:56:37 +0900

    Change destdir to current directory

--- scripts/build-debian-package.sh
+++ scripts/build-debian-package.sh
@@ -55,4 +55,3 @@ fakeroot debian/rules build
 fakeroot debian/rules binary
 # workaround. external/libpng will be dirty after making debian packages.
 env --chdir=external/libpng git reset --hard
-mv ../cavif_*.deb ../cavif-dbgsym_*.ddeb .
