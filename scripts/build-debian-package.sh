25d2511d4e79ef4771eac28c8a983dfd3c9ef17a
psi <ryo.hirafuji@link-u.co.jp>
2020-06-29 16:02:30 +0900

    remove sudo.

--- scripts/build-debian-package.sh
+++ scripts/build-debian-package.sh
@@ -23,7 +23,7 @@ EOF
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https ca-certificates gnupg software-properties-common wget
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
 
 # Install deps to build.
 mk-build-deps --install --remove \
