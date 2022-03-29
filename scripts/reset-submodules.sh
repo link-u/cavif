c2a941a36761e4947af6789982fb6c12d1ba857e
psi <ryo.hirafuji@link-u.co.jp>
2022-03-29 15:54:39 +0900

    add reset-submodule script

---
+++ scripts/reset-submodules.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -eux
+
+function readlink_f() {
+  local src='import os,sys;print(os.path.realpath(sys.argv[1]))'
+  python3 -c "${src}" "$1" || python -c "${src}" "$1"
+}
+
+ROOT_DIR="$(cd "$(readlink_f "$(dirname "$0")")" && cd .. && pwd)"
+cd "${ROOT_DIR}" || exit 1
+
+set -eux
+set -o pipefail
+
+git submodule foreach 'git reset --hard && git clean -fdx'
